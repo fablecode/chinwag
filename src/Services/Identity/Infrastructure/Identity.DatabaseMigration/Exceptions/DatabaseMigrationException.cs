b467487d45fae3a95644d6e81b02f11a5b923336
fablecode <fablecode@users.noreply.github.com>
2021-06-20 08:55:38 +0200

    DbUp and Identity tables added.

---
+++ src/Services/Identity/Infrastructure/Identity.DatabaseMigration/Exceptions/DatabaseMigrationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Identity.DatabaseMigration.Exceptions
+{
+    public sealed class DatabaseMigrationException : Exception
+    {
+        public DatabaseMigrationException()
+        {
+        }
+
+        public DatabaseMigrationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public DatabaseMigrationException(string? message) : base(message)
+        {
+        }
+
+        public DatabaseMigrationException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
