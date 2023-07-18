19579a3fbca8451644257ef4b25bd0942a9b563d
Dennis Poulton <dennis.poulton@honeywell.com>
2023-07-18 22:05:26 +0200

    Bare minimum Prism application

---
+++ src/Presentation/chinwag/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+using System.Windows;
+using chinwag.Views;
+
+namespace chinwag
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //throw new NotImplementedException();
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+    }
+}
