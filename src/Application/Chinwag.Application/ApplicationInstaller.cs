f0337116bae05417986ac5a0ed81225f7d89825f
Dennis Poulton <poultondennis@hotmail.com>
2023-08-10 23:07:53 +0200

    Application project layer

---
+++ src/Application/Chinwag.Application/ApplicationInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Ioc;
+
+namespace Chinwag.Application
+{
+    public static class ApplicationInstaller
+    {
+        public static IContainerRegistry AddApplicationServices(this IContainerRegistry containerRegistry)
+        {
+            return containerRegistry;
+        }
+    }
+}
